ed65f8e824159fcc33c75a4af6814eedac8f0de6
laveez <niko.muukkonen@gofore.com>
2021-11-09 16:08:09 +0200

    Remove dead code

--- src/App.js
+++ src/App.js
@@ -5,32 +5,6 @@ import List from './Components/List';
 import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  /*
-  const [items, setItems] = useState([]);
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    const getItems = async () => {
-      const res = await fetch(`https://trafi2.stat.fi/PXWeb/api/v1/fi/TraFi${path}?query=*&filter=*`);
-      const itemsFromServer = await res.json();
-      setItems(itemsFromServer);
-    }
-
-    getItems().then();
-  }, [path]);
-
-  const handleChange = event => {
-    setFilter(event.target.value);
-  };
-
-  const lowerCaseFilter = filter.toLowerCase();
-
-  const filteredData = items.filter(item => {
-    return Object.keys(item).some(key =>
-      typeof item[key] === "string" && item[key].toLowerCase().includes(lowerCaseFilter)
-    );
-  });
-*/
   return (
     <div className="App">
       <Routes>
